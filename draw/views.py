7550e8478c66d1bd98c09c6647cb160ec11cf788
sarkarchandro <sarkarchandro@berkeley.edu>
2018-07-16 21:24:04 -0400

    first try

--- draw/views.py
+++ draw/views.py
@@ -5,6 +5,9 @@ import json
 def index(request):
     return render(request, 'draw/index.html', {})
 
+def custom(request):
+    return render(request, 'draw/custom.html', {})  
+
 def room(request, room_name):
     return render(request, 'draw/room.html', {
         'room_name_json': mark_safe(json.dumps(room_name))
--- p4/urls.py
+++ p4/urls.py
@@ -4,4 +4,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('draw/', include('draw.urls')),
+    path('draw/custom/', include('draw.urls')),
 ]
